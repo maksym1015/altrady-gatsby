4d8f6998759be52286c6ac00306471e204fc257a
max9871 <max.petrivskyi@gmail.com>
2021-04-27 11:10:11 +0300

    add some bootstrap styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello Bootstrap!</div>
+  return (
+    <div className="container">
+      <p className="text-center mt-4">Hello Bootstrap!</p>
+    </div>
+  )
 }
